64e23231bdb110d8227299a51ddae865e2414a70
Gojanovic <GY28@millercoors.com>
2018-04-02 17:58:21 -0600

    one more time

---
+++ tests/testthat/test_FARS.R
@@ -0,0 +1,11 @@
+context("Test the map function of the FARS package")
+
+library(dplyr)
+library(maps)
+library(FARS)
+
+test_that("fars_map_state() works correctly", {
+     expect_silent(fars_map_state(13, 2014))           
+     expect_silent(fars_map_state(6, 2013))           
+     expect_error(fars_map_state(1, 2016))             
+   })
