937b8cf313ac69154d6fb5edae7cff5e794fcc54
Gojanovic <GY28@millercoors.com>
2018-04-02 09:21:54 -0600

    second attempt

--- R/fars_project.R
+++ R/fars_project.R
@@ -2,9 +2,10 @@
 # Coursera "Building R Packages"
 # Final project
 # April 2018
-#
+
 #' Reading a FARS data set
-#'
+#' 
+#' @description
 #' This function reads in data set from the NHTSA Fatality Analysis Reporting System (FARS).
 #' @param filename FARS character filename in csv format to be read. If the filename is incorrect or not located, a message is issued.
 #' @return Returns a data frame from a csv file in tabular format.
@@ -12,7 +13,8 @@
 #' @importFrom dplyr tbl_df
 #' @importFrom readr read_csv
 #' @references  National Highway Traffic Safety Administration's Fatality Analysis Reporting System data set. https://www.nhtsa.gov/research-data/fatality-analysis-reporting-system-fars
-#' @export 
+#' @export
+
 fars_read <- function(filename) {
   if(!file.exists(filename))
     stop("file '", filename, "' does not exist")
@@ -24,22 +26,19 @@ fars_read <- function(filename) {
 
 #' Creating a file name
 #'
-#' This function creates a data file name based on the selected year.
 #' @param year Year of interest which is numeric value.
 #' @return Returns a character vector file name based on the selected year and a bz2 extendsion.
 #' @note The year will be formatted as an integer.
 #' @details The function uses sprintf is a wrapper for the system sprintf C-library function. Attempts are made to check that the mode of the values passed match the format supplied, and R's special values (NA, Inf, -Inf and NaN) are handled correctly.
 #' @export
 
-make_filename <- function(year) {                 ## Used this code fragment from ty byers suggestion and code 
-  year <- as.integer(year)                        ## Assures the input is an integer value
-  filename <- sprintf("accident_%d.csv.bz2", year)
+make_filename <- function(year) {
+  year <- as.integer(year)
+  sprintf("accident_%d.csv.bz2", year)
 }
-
 
 #' Create new data files from the FARS data
 #'
-#' Read the desired yearly data file and create a new data set with two variables, month and year.
 #' @param years Desired range of years as numeric values. If a year is incorrect, a message is issued.
 #' @return Returns a filtered list of dataframes with a year and month variable or NULL if it doesn't exist.
 #' @importFrom dplyr mutate select %>%
@@ -47,24 +46,23 @@ make_filename <- function(year) {                 ## Used this code fragment fro
 #' @details Uses the function make_filename and fars_read
 #' @export
 
+
 fars_read_years <- function(years) {
-  lapply(years, function(year) {                  ## Applies the year range to the function to make new files based on each year.
-    file <- make_filename(year)                   ## Creates a new filename with the correct format to be read
+  lapply(years, function(year) {
+    file <- make_filename(year)
     tryCatch({
-      dat <- fars_read(file)                      ## Reads in the correct year file to a new file.
-      dplyr::mutate(dat, YEAR = YEAR) %>%         ## Creates a new dat file based on the year values
-        dplyr::select(MONTH, YEAR)                ## Selects the month and year for the year file.
-    }, error = function(e) {                      ## Error routine and messaging for improper dates.
+      dat <- fars_read(file)
+      dplyr::mutate(dat, year = year) %>% 
+        dplyr::select(MONTH, year)
+    }, error = function(e) {
       warning("invalid year: ", year)
       return(NULL)
     })
   })
 }
 
-
 #' Summary of accident counts by years and month
 #'
-#' This function creates a simple tabular summary of accident counts by year and month.
 #' @param years Desired range of years as numeric input.
 #' @return Returns a summary dataframe by year (column) and month (row) with a summary numeric count of accidents.
 #' @importFrom dplyr bind_rows group_by summarize %>%
@@ -74,16 +72,15 @@ fars_read_years <- function(years) {
 #' @export
 
 fars_summarize_years <- function(years) {
-    dat_list <- fars_read_years(years)            ## creates a new file based on year range and the function fars_read_years.
-    dplyr::bind_rows(dat_list) %>%                ## Use dplyr to creat a new data file with all the years
-     dplyr::group_by(YEAR, MONTH) %>%       ## Use the grouping variable to provide the summary by year and month
-     dplyr::summarize(n = n()) %>%            ## Summarize counts by year and month
-     tidyr::spread('YEAR', 'n')                 ## Create a table with years as columns, rows as months and summary of accidents.
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>% 
+    dplyr::group_by(year, MONTH) %>% 
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
 }
 
 #' Plot of accidents by geographic location
 #'
-#' This function provides a geographic summary of accident locations with a simple state map.
 #' @param state.num  The desired state field which becomes an integer input.
 #' @param year  The desired year field which becomes an integer input.
 #' @importFrom maps map
@@ -96,22 +93,22 @@ fars_summarize_years <- function(years) {
 #' @export
 
 fars_map_state <- function(state.num, year) {
-  filename <- make_filename(year)                         ## Creates the filename by year to be accessed.
-  data <- fars_read(filename)                             ## Read the correct file
-  state.num <- as.integer(state.num)                      ## Assure the state number is an integer value
-
-  if(!(state.num %in% unique(data$STATE)))                ## Error reporting if state number is not in the database
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+  
+  if(!(state.num %in% unique(data$STATE)))
     stop("invalid STATE number: ", state.num)
-  data.sub <- dplyr::filter(data, STATE == state.num)  ## If state number is located, filter on the state number
-  if(nrow(data.sub) == 0L) {                              ## Reporting of no data found
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
     message("no accidents to plot")
     return(invisible(NULL))
   }
-  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900 ## Else find the longtitude
-  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90  ## Find the latitude
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
   with(data.sub, {
-    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),  ## State map
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
               xlim = range(LONGITUD, na.rm = TRUE))
-    graphics::points(LONGITUD, LATITUDE, pch = 46)            ## Plot the accident location with a point
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
   })
 }
