0cd982d7a765741c2b5228143f8849079928b846
Gojanovic <GY28@millercoors.com>
2018-04-01 20:19:43 -0600

    modify test routine

--- tests/testthat/test_FARS.R
+++ tests/testthat/test_FARS.R
@@ -5,7 +5,12 @@ library(maps)
 library(FARS)
 
 
-test_that("fars_map_state() works correctly", {
+test_that("fars_map_state() works", {
      expect_silent(fars_map_state(13, 2014))           
      expect_silent(fars_map_state(6, 2013))           
+})
+
+test_that("fars_read() works", {
+  expect_is(fars_read("accident_2013.csv.bz2"), "tbl_df")
+  expect_error(fars_read("accident_2012.csv.bz2"))
 })
